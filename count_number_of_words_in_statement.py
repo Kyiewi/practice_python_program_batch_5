ff8fa8c76c41b3fc0310b3c1ae65ef000c9ebfc3
Kyiewi <kyrie.norriega@gmail.com>
2025-03-21 21:37:06 +0800

    Program that counts the number of words of a user input statement

---
+++ count_number_of_words_in_statement.py
@@ -0,0 +1,11 @@
+#Ask User to enter a complete statement
+statement = input("Enter a complete statement: ")
+
+#Split the statement into words
+words = statement.split()
+
+#Count the words
+word_count = len(words)
+
+# Print Output
+print("Total number of words:", word_count )
