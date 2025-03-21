d6bae9a6066d735477b655b9e6c337ca56e586a2
Kyiewi <kyrie.norriega@gmail.com>
2025-03-21 21:53:40 +0800

    updated user prompt on multiple programs

--- convert_user_name_to_proper_casing.py
+++ convert_user_name_to_proper_casing.py
@@ -1,5 +1,5 @@
 #Ask User to input their full name
-user_name = input("Enter your full name: ")
+user_name = input("Enter your full name in incorrect casing: ")
 
 #Convert to proper casing and Print Output
 print("Name:", user_name.title())
--- format_user_name_to_lowercase.py
+++ format_user_name_to_lowercase.py
@@ -1,5 +1,5 @@
 #Ask User to input their full name
-user_name = input("Enter your full name: ")
+user_name = input("Enter your full name in incorrect casing: ")
 
 #Format name to lowercase and Print Output
 print("Name:", user_name.lower())
