1c1e61475b72fc67e60153e9051be597716e97b2
Kyiewi <kyrie.norriega@gmail.com>
2025-03-21 22:00:18 +0800

    program that counts the characters of user input name

---
+++ count_number_of_characters_in_name.py
@@ -0,0 +1,8 @@
+#Ask User to input their full name
+user_name = input("Enter Full Name: ")
+
+#Count the characters
+char_count = len(user_name)
+
+# Print Output
+print("Total number of characters:", char_count)
