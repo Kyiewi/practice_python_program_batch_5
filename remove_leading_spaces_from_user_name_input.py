b0180b41c50faba3afefcab12ed7120ebff905e9
Kyiewi <kyrie.norriega@gmail.com>
2025-03-21 17:39:41 +0800

    Cleaned up code formatting and removed redundancy

--- remove_leading_spaces_from_user_name_input.py
+++ remove_leading_spaces_from_user_name_input.py
@@ -1,5 +1,5 @@
 #Ask User to input their full name
-user_name= input(str("Enter your full name with spaces in the beginning: "))
+user_name = input("Enter your full name with spaces in the beginning: ")
 
 #Remove spaces and Print Output
-print("Name:",user_name.strip())
+print("Name:", user_name.strip())
